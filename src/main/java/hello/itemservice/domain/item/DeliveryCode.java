32cc279c9142724a2eae4120cea9d4b5d4c82f85
KDH_Notebook <destiny1017@nate.com>
2022-07-19 11:58:12 +0900

    Thymeleaf 입력 폼 처리, 요구사항 추가

---
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -0,0 +1,13 @@
+package hello.itemservice.domain.item;
+
+/**
+ * FAST : 빠른배송
+ * NORMAL : 일반배송
+ * SLOW : 느린배송
+ */
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
+
+}
